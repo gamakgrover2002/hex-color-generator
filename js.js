7d14514b2cf08ca6eab9db6567cc25a6856d80a1
gamak grover <112484328+gamakgrover2002@users.noreply.github.com>
2022-10-27 10:50:46 +0530

    Add files via upload

--- js.js
+++ js.js
@@ -1,8 +1,7 @@
 function color(){
     const x = 0;
-    const cars =[];
     const randomNumber =Math.floor(Math.random()*1677215);
-    let code= "#" + randomNumber.toString(16);
+    const code= "#" + randomNumber.toString(16);
     document.body.style.backgroundColor = code;
     document.getElementById('color-code').innerText = code;
     
